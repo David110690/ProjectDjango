12d9b58bed51f06ae3a40bf218c2d0ceedbaa133
David Travezano <Davidu.0622@gmail.com>
2021-01-18 19:23:20 -0500

    Commit2

--- RedSocial/settings.py
+++ RedSocial/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ubi4-1+9=c51__#63&nb3p$e)96+@q9omobf0@t_o&m^=p687('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "DevTrave.pythonanywhere.com"]
 
 
 # Application definition
